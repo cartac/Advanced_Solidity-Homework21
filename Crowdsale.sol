0f22624e1d26326e225d104ade19e4d006b7c751
Carlitos <ctacchi@gmail.com>
2021-03-05 01:57:15 -0500

    upload files and readme

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.5;
 
 import "./PupperCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
@@ -8,14 +8,16 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 // @TODO: Inherit the crowdsale contracts
-contract PupperCoinSale is {
+contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
 
-    constructor(
-        // @TODO: Fill in the constructor parameters!
-    )
-        // @TODO: Pass the constructor parameters to the crowdsale contracts.
-        public
-    {
+    // potentially remove
+    constructor(uint rate, address payable wallet, PupperCoin token, uint256 goal, uint256 openingTime, uint256 closingTime) 
+    Crowdsale(rate, wallet, token)
+    CappedCrowdsale(goal)
+    TimedCrowdsale(openingTime, closingTime)
+    RefundableCrowdsale(goal)
+    //MintedCrowdsale()
+    public {
         // constructor can stay empty
     }
 }
@@ -24,16 +26,27 @@ contract PupperCoinSaleDeployer {
 
     address public token_sale_address;
     address public token_address;
+    
 
     constructor(
-        // @TODO: Fill in the constructor parameters!
-    )
-        public
+        string memory name,
+        string memory symbol,
+        address payable wallet,
+        uint initial_supply,
+        uint256 goal
+        //uint256 openingTime,
+        //uint256 closingTime,
+        ) public
     {
-        // @TODO: create the PupperCoin and keep its address handy
+        // @TODO: create the PupperCoin and keep its address handy. //
+        PupperCoin token = new PupperCoin(name, symbol, 0);
+        token_address = address(token);
 
         // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
-
+        PupperCoinSale token_sale = new PupperCoinSale(1, wallet, token, goal, now, now + 10 minutes);
+        token_sale_address = address(token_sale);
+        
+        
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
         token.renounceMinter();
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Homework 21: You sure can attract a crowd!
-## Deploying the Crowdsale
+## Deploying the CrowdSale
 
-1.	Set the network as Kovan
+**1.	Set the network as Kovan**
 
 ![address](Screenshots/01.Kovan.png)
 
-2.	Deploy CrowdSale Contract
+**2.	Deploy CrowdSale Contract**
 
 After compiling the files, we deploy the Pupper Coin CrowdSale Deployer
 * We determine the name and symbol of the coin, the wallet behind it as well as the initial supply and the goal, which is set to be the same as the cap.
@@ -15,14 +15,14 @@ After compiling the files, we deploy the Pupper Coin CrowdSale Deployer
 After hitting confirm in MetaMax, we have successfully deployed the contract with the various contracts that, after being deployed, will allow us to create the coin and make coin sales.
 
 
-3.	Deploy Pupper Coin Contract
+**3.	Deploy Pupper Coin Contract**
 
 From the contract deployed in the prior step we will use the token address generated to input in this step. 
 
 ![address](Screenshots/03.DeployingPupperCoin.png)
 
 
-4.	Deploy Pupper Coin Sale Contract
+**4.	Deploy Pupper Coin Sale Contract**
 
 From the first contract deployed, we will use the token sale address generated to input in this step. 
 
@@ -34,18 +34,22 @@ Before we move to the next step, we confirm that all the contracts have been dep
 ![address](Screenshots/05.AllContractsDeployed.png)
 
 
-Buy ARGIE 
+**5.Buy ARGIE**
 We will buy from a different account to the account that created the coin, 0.05 ETH + 1 WEI to hit the goal and an extra.
-![address](Screenshots/06.PurchaseofCoins.png)
 ![address](Screenshots/06.PurchaseofCoinsv1.png)
 
-And now, we wait until the crowd sale finishes. 
+**6. And now, we wait until the crowd sale finishes**
 
 ![address](Screenshots/07.WaitTime.png)
 
-We can now finalize the auction and claim our token. 
+**7. We can now finalize the auction and claim our token**
+
 ![address](Screenshots/08.Finalize.png)
 
-Add Token to the Wallet and wait until we see the balance!
+
+**8. Add Token to the Wallet and wait until we see the balance!**
 
 ![address](Screenshots/09.AddToken.png)
+
+
+And this is how we successfully sold 0.05 ETH (less than the required amount but due to limited ETH available in the multiple accounts) of ARGIE.
